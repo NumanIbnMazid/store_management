bae7f44ec4cd58cd204c0929bb27fdd64d17854b
NumanIbnMazid <numanibnmazid@gmail.com>
2021-09-26 11:10:03 +0600

    Initial Commit for Heroku

--- studio_reservation/settings/heroku.py
+++ studio_reservation/settings/heroku.py
@@ -2,10 +2,10 @@
 # import common configurations
 from studio_reservation.settings.common import *
 import django_heroku
+import dj_database_url
 
 """ *** Application Allowed Hosts *** """
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["studio-reservation.herokuapp.com"]
 
 """ *** Database Configuration *** """
 if env.str('DATABASE_URL', default=''):
@@ -19,6 +19,10 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Activate Django-Heroku.
