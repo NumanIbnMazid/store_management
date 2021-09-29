7f98fdd7b854ad41d99dd9f7d0f5918b3cd3ddb9
NumanIbnMazid <numanibnmazid@gmail.com>
2021-09-29 17:58:55 +0600

    static path changed

--- studio_reservation/settings/common.py
+++ studio_reservation/settings/common.py
@@ -124,9 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_APP_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_APP_DIR, 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
