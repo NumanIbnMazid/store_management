09899bc980315bc899f46d934659e9b89547cd3a
NumanIbnMazid <numanibnmazid@gmail.com>
2021-09-30 15:55:36 +0600

    URL endpoint updated

--- studio_calendar/api/urls.py
+++ studio_calendar/api/urls.py
@@ -8,6 +8,6 @@ router = DefaultRouter()
 urlpatterns = [
     # ==============================*** Studio Calendar URLS ***==============================
     path("get-single-holiday-status/", StudioCalendarManagerViewSet.as_view({"post": "check_single_holiday"}, name="check_single_holiday")),
-    path("get-holiday-from-range/", StudioCalendarManagerViewSet.as_view({"post": "check_holiday_between_range"}, name="check_holiday_between_range")),
-    path("get-all-holidays-by-year/", StudioCalendarManagerViewSet.as_view({"post": "check_holidays_for_year"}, name="check_holidays_for_year")),
+    path("get-holidays-by-range/", StudioCalendarManagerViewSet.as_view({"post": "check_holiday_between_range"}, name="check_holiday_between_range")),
+    path("get-holidays-by-year/", StudioCalendarManagerViewSet.as_view({"post": "check_holidays_for_year"}, name="check_holidays_for_year")),
 ]
